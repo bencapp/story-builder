330126ecedaa6d9fcf7004317a9a81d1eb2b3518
Ben Capp <bentucker333@gmail.com>
2023-03-24 08:55:17 -0500

    completed

--- database.sql
+++ database.sql
@@ -11,6 +11,40 @@ CREATE TABLE "user" (
 
 CREATE TABLE "invite" (
     "id" SERIAL PRIMARY KEY,
-    "user1_id" INT REFERENCES "user" ON DELETE CASCADE,
-    "user2_id" INT REFERENCES "user" ON DELETE CASCADE
+    "sender_user_id" INT REFERENCES "user" ON DELETE CASCADE,
+    "recipient_user_id" INT REFERENCES "user" ON DELETE CASCADE,
+    "title" VARCHAR (80),
+    "speed_type" VARCHAR(20),
+    "text_type" VARCHAR(20)
+);
+
+CREATE TABLE "story" (
+	"id" SERIAL PRIMARY KEY,
+	"title" VARCHAR(80),
+	"speed_type" VARCHAR(50),
+	"length_type" VARCHAR(50),
+	"public" BOOLEAN DEFAULT FALSE,
+	"current_user_turn_id" INT,
+	"start_time" TIMESTAMP,
+);
+
+CREATE TABLE "user_story" (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" INT REFERENCES "user",
+	"story_id" INT REFERENCES "story"
+);
+
+CREATE TABLE "user_story_votes" (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" INT REFERENCES "user",
+	"story_id" INT REFERENCES "story",
+	"vote" INT DEFAULT 0
+);
+
+CREATE TABLE "text" (
+	"id" SERIAL PRIMARY KEY,
+	"story_id" INT REFERENCES "story",
+	"user_id" INT REFERENCES "user",
+	"text" VARCHAR(255),
+	"timestamp" TIMESTAMP
 );
